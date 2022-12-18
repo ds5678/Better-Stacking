8f914b8c3296d7ca01b28127b09054bf23138a05
Dominic Rogers <dommrogers@gmail.com>
2022-12-18 20:14:31 +0000

    TODO - fix removal of gearToAdd to avoid duplication

--- BetterStacking/Patches.cs
+++ BetterStacking/Patches.cs
@@ -29,20 +29,14 @@ namespace BetterStacking
             internal static bool Prefix(PlayerManager __instance, GearItem gearToAdd, float normalizedCondition, int numUnits, out GearItem existingGearItem)
             {
 
-                //MelonLogger.Msg("PlayerManager.TryAddToExistingStackable | " + gearToAdd.name);
-                //MelonLogger.Msg("PlayerManager.TryAddToExistingStackable | " + normalizedCondition);
-                //MelonLogger.Msg("PlayerManager.TryAddToExistingStackable | " + numUnits);
-
                 if (normalizedCondition == 0)
                 {
-                    //MelonLogger.Msg("normalizedCondition == 0 ");
                     existingGearItem = null;
                     return false;
                 }
 
                 if (Implementation.UseDefaultStacking(gearToAdd))
                 {
-                    //MelonLogger.Msg("UseDefaultStacking");
                     existingGearItem = null;
                     return true;
                 }
@@ -50,20 +44,13 @@ namespace BetterStacking
                 GearItem targetStack = GameManager.GetInventoryComponent().GetClosestMatchStackable(gearToAdd.GearItemData, normalizedCondition);
                 if (!Implementation.CanBeMerged(targetStack, gearToAdd))
                 {
-                    //MelonLogger.Msg("CanBeMerged NOT | "+ targetStack.name + " | "+gearToAdd.name);
                     existingGearItem = null;
                     return false;
                 }
 
-                //MelonLogger.Msg("CanBeMerged | " + targetStack.name + " | " + gearToAdd.name);
-                // should only destroy if we have an existing stack in inventory
                 if (targetStack.m_StackableItem != null)
                 {
-                    //MelonLogger.Msg("existing stack | " + targetStack.name + " | " + gearToAdd.name);
-                    //MelonLogger.Msg("CanBeMerged item | " + targetStack.m_StackableItem + " | " + gearToAdd.m_StackableItem);
-                    //MelonLogger.Msg("CanBeMerged counts | " + targetStack.m_StackableItem.m_Units + " | " + gearToAdd.m_StackableItem.m_Units);
                     Implementation.MergeIntoStack(normalizedCondition, numUnits, targetStack);
-                    GameManager.GetInventoryComponent().DestroyGear(gearToAdd.gameObject);
                     existingGearItem = null;
                     return false;
                 }
