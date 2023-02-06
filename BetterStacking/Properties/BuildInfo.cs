bc694d8b10b1f4cde23e5ba39142764ef78a5875
STBlade <dommrogers@gmail.com>
2023-02-06 23:59:41 +0000

    bump version to 5.0.0

--- BetterStacking/Properties/BuildInfo.cs
+++ BetterStacking/Properties/BuildInfo.cs
@@ -6,7 +6,7 @@
         public const string Description = "A mod for making items stack neater."; // Description for the Mod.  (Set as null if none)
         public const string Author = "WulfMarius, ds5678"; // Author of the Mod.  (MUST BE SET)
         public const string Company = null; // Company that made the Mod.  (Set as null if none)
-        public const string Version = "4.1.2"; // Version of the Mod.  (MUST BE SET)
+        public const string Version = "5.0.0"; // Version of the Mod.  (MUST BE SET)
         public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
     }
 }
